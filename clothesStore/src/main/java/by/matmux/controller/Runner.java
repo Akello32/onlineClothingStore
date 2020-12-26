88ae153b8ce42dadf1c13e33b32b012d06cd4300
mustafarara <matmux23@gmail.com>
2020-12-26 17:56:01 +0300

    maybe complete DAO

--- clothesStore/src/main/java/by/matmux/controller/Runner.java
+++ clothesStore/src/main/java/by/matmux/controller/Runner.java
@@ -1,9 +1,25 @@
 package by.matmux.controller;
 
+import java.math.BigDecimal;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import by.matmux.bean.Clothes;
+import by.matmux.dao.mysql.ClothesDaoImpl;
 
 public class Runner {	
+	private static final Logger log = LogManager.getLogger(Runner.class);
+
 	public static void main(String[] args) {
-		
+		ClothesDaoImpl impl = new ClothesDaoImpl();
+		Clothes clothes = new Clothes();
+		clothes.setBrandId(1);
+		clothes.setColor("red");
+		clothes.setNumbers(3);
+		clothes.setPrice(new BigDecimal(45));
+		clothes.setSize("Xl");
+		clothes.setTypeId(1);
+		log.debug(impl.create(clothes));
 	}
 }
